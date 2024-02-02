73723939758190d84adc6174ce0909985a5900d0
tavonline <35136091+tavonline@users.noreply.github.com>
2024-02-03 02:02:37 +0300

    popup image

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,21 +27,29 @@ $(document).ready(function(){
   var sliderAnime = gsap.timeline({yoyo: false, reversed: true});
   sliderAnime.pause();
 
-  sliderAnime.to($('.hero-slider .slide.active'), {right: $(this).css('right'), width: '9%', position: 'absolute'}, 0.1);
-  sliderAnime.set($('.hero-slider .slide.active'), {left:'auto'}, 0.2);
-  sliderAnime.to($('.hero-slider .slide.active').find('.slide-content'), {autoAlpha:0, position:'absolute' }, 0.1);
-  sliderAnime.to($('.hero-slider .slide.active').find('.image'), {'border-radius':'35px', width:'100%', duration: 0.5 }, 0.1);
+  sliderAnime.to($('.hero-slider .slide.active').find('.slide-content'), {'opacity':0, duration:.1, ease: "Expo.easeInOut",},0);
+  sliderAnime.to($('.hero-slider .slide.active'), {width: '9%', duration:1, ease: "Expo.easeInOut",},0.5);
+  sliderAnime.to($('.hero-slider .slide.active').find('.popup-image'), {x:-50, autoAlpha:0, duration:.4},.9);
+  sliderAnime.set($('.hero-slider .slide.active'), { right: $(this).css('right'),  position: 'absolute',},.9);
+  sliderAnime.to($(this), {width: '60%', left: 0, duration: 0.5 },0.9);
+   sliderAnime.set($('.hero-slider .slide.active'), {left:'auto',},.9);
+   sliderAnime.to($('.hero-slider .slide.active').find('.image'), {'border-radius':'35px', width:'100%', duration: 0.5 },.9);
   
-  sliderAnime.to($(this), {width: '60%', left: 0, duration: 0.5 }, 0.1);
-  sliderAnime.to($(this).find('.slide-content'), {width: '50%', duration: 0.5 }, 0.1);
-  sliderAnime.to($(this).find('.image'), {width: '50%', float: 'right', 'border-radius': '0', duration: 0.5 }, 0.1);
-  sliderAnime.to($(this).find('.slide-content'), {position: 'relative', autoAlpha: 1, duration: 0.5 });
-  sliderAnime.from($(this).find('.slide-content h1'), {x:-100, autoAlpha:0, duration: 0.5 });
+   sliderAnime.to($(this).find('.slide-content'), {width: '45%', duration: 0.5 },.9);
+   sliderAnime.to($('.hero-slider .slide.active').find('.slide-content'), {'position':'absolute', ease: "Expo.easeInOut",},0.9);
+   sliderAnime.to($(this).find('.image'), {width: '45%', float: 'right', 'border-radius': '0', duration: 0.5 },.9);
+   sliderAnime.to($(this).find('.slide-content'), {position: 'relative', autoAlpha: 1, duration: 0.5 },1.5);
+   sliderAnime.to($(this).find('.popup-image'), {x:0, autoAlpha:1, duration:.5},1.5);
+   sliderAnime.from($(this).find('.slide-content .item'), {x:-100, autoAlpha:0, stagger:0.2, },2);
 
   sliderAnime.play();
 
-  $('.hero-slider .slide').removeClass('active');
-  $(this).addClass('active');
+  setTimeout(() => {
+    $('.hero-slider .slide').removeClass('active');
+    $(this).addClass('active');
+  }, 1400);
+
+
         });
 
 });
